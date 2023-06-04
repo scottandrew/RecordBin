7cfb02cb708907649e6a2ca7029ab08314fa5267
Scott Andrew <scott@nwdm.tech>
2023-06-04 09:06:44 -0700

    Initial Commit

---
+++ RecordBin/RecordBinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordBinApp.swift
+//  RecordBin
+//
+//  Created by Scott Andrew on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordBinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
